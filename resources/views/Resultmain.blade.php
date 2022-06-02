29a027509f9ebd45aef07da3f6dc5da3fa36d917
nex124 <105812216+nex124@users.noreply.github.com>
2022-06-03 07:09:20 +0800

    First Commit

---
+++ resources/views/Resultmain.blade.php
@@ -0,0 +1,37 @@
+@extends('master')
+
+@section('table')
+<center>
+<h1>FYP CARNIAL MARKS</h1>
+
+<center>
+<table class="table" style="width:75%">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">NAME</th>
+      <th scope="col">MATRIX ID</th>
+      <th scope="col">SUPERVISOR</th>
+      <th scope="col">FINAL YEAR PROJECT TITLE</th>
+      <th scope="col">TOTAL MARKS</th>
+      <th scope="col">ASSIGN INDUSTRY</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td></td>
+    </tr>
+  </tbody>
+</table>
+<center>
+
+@endsection
---
+++ resources/views/assign_indus.blade.php
@@ -0,0 +1,33 @@
+@extends('master')
+
+@section('form')
+
+
+<form>
+<div class="center">  
+  <div class="form-group row">
+    <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="inputEmail3" placeholder="Email" style="width:1000px">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword3" placeholder="Password" style="width:1000px">
+    </div>
+  </div>
+
+  <div class="form-group row">
+    <label for="exampleFormControlTextarea1" class="col-sm-2 col-form-label">Example textarea</label>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" style="width:500px; height: 100px;"></textarea>
+  </div>
+  <div class="form-group row">
+    <div class="col-sm-10">
+      <button type="submit" class="btn btn-primary">Sign in</button>
+    </div>
+  </div>
+  </div>
+</form>
+  <center>
+@endsection
