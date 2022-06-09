bda07d638b32ee810698a1e9bbd94ff7499bcf81
nightblue99 <73087186+nightblue99@users.noreply.github.com>
2022-06-10 06:42:10 +0800

    updated

--- resources/views/CarnivalEvaluation/edit.blade.php
+++ resources/views/CarnivalEvaluation/edit.blade.php
@@ -8,18 +8,10 @@
         <h2>HTML Forms</h2>
 
         <form action="/action_page.php">
-            <label for="fname">Cooordinator ID:</label><br>
-            <input type="text" id="fname" name="fname" value="John"><br>
-            <label for="lname">Coordinator Name:</label><br>
-            <input type="text" id="lname" name="lname" value="Doe"><br><br>
-            <label for="lname">Student Name:</label><br>
-            <input type="text" id="lname" name="lname" value="Doe"><br><br>
-            <label for="lname">Student Phone Number:</label><br>
-            <input type="text" id="lname" name="lname" value="Doe"><br><br>
+            <label for="fname">Appoinment ID:</label><br>
+            <input name="appoinmentID" type="string" placeholder="Type here"><br>
             <label for="lname">Date:</label><br>
-            <input type="text" id="lname" name="lname" value="Doe"><br><br>
-            <label for="lname">Reason:</label><br>
-            <input type="text" id="lname" name="lname" value="Doe"><br><br>
+            <input name="date" type="date" placeholder="Type here"><br>
             <input type="submit" value="Submit">
         </form>
 
